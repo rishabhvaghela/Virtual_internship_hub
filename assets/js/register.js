77090da8eab4a5d1da245695e1cf8c3144559e11
Rishabh <vaghelarishu6@gmail.com>
2026-01-30 18:12:50 +0530

    register.js added

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,25 +1,4 @@
     const roleSelect = document.getElementById('role');
-    const studentFields = document.getElementById('studentFields');
-    const companyFields = document.getElementById('companyFields');
-
-    studentFields.style.display = 'none';
-    companyFields.style.display = 'none';
-
-    roleSelect.addEventListener('change', () => {
-      // Hide all
-
-      // Show selected
-      const role = roleSelect.value;
-      if (role === 'student') {
-        studentFields.style.display = 'block';
-        companyFields.style.display = 'none';
-      }
-      else if (role === 'company') {
-        studentFields.style.display = 'none';
-        companyFields.style.display = 'block';
-      }
-    });
-    
 
 roleSelect.addEventListener("change", function() {
   let emailInput = document.getElementById("email");
